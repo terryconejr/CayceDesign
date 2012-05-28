7c76e77908bd4e7df6002aaf0b62d5e8db90f672
Terry Cone <terryconejr@yahoo.com>
2012-05-28 08:21:41 -0400

    First Commit for Cayce Design

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Caycedesign::Application.config.secret_token = '3371af1e7907ccb142eb0d4c8f457bb281053c69bb2aeb6bcf29cd40f468feeacf9847c2c50aa06d14ac5af08c52c445a3096899d4797de920050fc0398692c2'
