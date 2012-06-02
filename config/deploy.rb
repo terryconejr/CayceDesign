58d15683eae88e848bc3ae9ee799417714b1bbd2
Terry Cone <terryconejr@yahoo.com>
2012-06-02 17:16:05 -0400

    first deploy attempt

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,13 +2,31 @@ require 'capistrano'
 require 'capistrano-vexxhost'
 require 'bundler/capistrano'
 
+ssh_options[:forward_agent] = true
+
 # Account Settings
 set :user, "caycedes"
 set :password, "XpIcWtFHM"
 set :domain, "caycedes.caycedesign.com"
+set :deploy_to, "/home/caycedes/rails_apps/#{application}"
 set :mount_path, "/"
 set :application, "caycedesign"
-set :repository, "/Users/dev/code/deploy_app"
-set :local_repository, "/Users/dev/code/deploy_app"
 set :scm, :git
-set :deploy_via, :copy
+set :repository, "git@github.com:terryconejr/caycedesign.git"
+set :branch, "master"
+set :deploy_via, :remote_cache
+
+after "deploy", "deploy:bundle_gems"
+after "deploy:bundle_gems", "deploy:restart"
+
+# If you are using Passenger mod_rails uncomment this:
+ namespace :deploy do
+   task :bundle_gems do
+   	 run "cd #{deploy_to}/current && bundle install vendor/gems"
+   end
+   task :start do ; end
+   task :stop do ; end
+   task :restart, :roles => :app, :except => { :no_release => true } do
+     run "touch #{File.join(current_path,'tmp','restart.txt')}"
+   end
+ end
